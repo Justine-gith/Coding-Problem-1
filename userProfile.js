c84a6fa6fabe98ff5c00f5820d7740d5afebbd46
unknown <ganijustine8@gmail.com>
2024-11-04 17:30:11 +0800

    initial commit

---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Justine Gani";
+let age = "19";
+let favoriteNumber = "17";
+let favoriteColor = "red";
+
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
